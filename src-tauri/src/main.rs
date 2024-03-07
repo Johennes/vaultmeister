fd1fe275e452182b5d131fcb90c5d844b7d7ab50
Johannes Marbach <n0-0ne+github@mailbox.org>
2024-03-07 21:34:37 +0100

    Remove unused imports

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use matrix_sdk::{config::SyncSettings, ruma::{api::client::room::create_room::{self, v3::CreationContent}, room::RoomType, PrivOwnedStr}, Client};
+use matrix_sdk::{config::SyncSettings, ruma::{api::client::room::create_room::{self, v3::CreationContent}}, Client};
 use std::{process, sync::Arc, thread};
 use tauri::Url;
 
